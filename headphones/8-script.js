a41a5666e0b8f7f0135877e5350fb0d8411ce4aa
Chioma <okunborabigail@gmail.com>
2025-12-07 22:47:41 +0000

    Add hamburger menu toggle functionality

---
+++ headphones/8-script.js
@@ -0,0 +1,6 @@
+// Toggle hamburger menu
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
